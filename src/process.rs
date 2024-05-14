0f5f0b2717e01c1101dbbbd4d553a44916529395
joyforever <joyforever@aliyun.com>
2024-05-14 23:35:41 +0800

    支持任意文件转换

--- src/process.rs
+++ src/process.rs
@@ -1,27 +1,19 @@
 use csv::Reader;
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Deserialize, Serialize)]
-#[serde(rename_all = "PascalCase")]
-struct Player {
-    name: String,
-    position: String,
-    #[serde(rename = "DOB")]
-    dob: String,
-    nationality: String,
-    #[serde(rename = "Kit Number")]
-    kit: u8,
-}
+use serde_json::Value;
 
 pub fn process_csv(input: &str, output: &str) -> anyhow::Result<()> {
     let mut reader = Reader::from_path(input)?;
-    let mut players = Vec::with_capacity(100);
-    for player in reader.deserialize::<Player>() {
-        let player = player?;
-        players.push(player);
+
+    let headers = reader.headers()?.clone();
+    let mut records = Vec::with_capacity(128);
+
+    for result in reader.records() {
+        let record = result?;
+        let json_value = headers.iter().zip(record.iter()).collect::<Value>();
+        records.push(json_value);
     }
 
-    let json = serde_json::to_string_pretty(&players)?;
+    let json = serde_json::to_string_pretty(&records)?;
     std::fs::write(output, json)?;
 
     Ok(())
